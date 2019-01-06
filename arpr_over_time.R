240c80faf22a00cfc3ef5fbe5f0cf233773b7782
Casputin <cengelen0@gmail.com>
2019-01-06 16:43:01 +0100

    #8 Added "At-risk-of-poverty-rate" over time.

---
+++ arpr_over_time.R
@@ -0,0 +1,39 @@
+library(readr)
+library(ggplot2)
+library(plotly)
+library(eurostat)
+library(lubridate)
+library(dplyr)
+
+input <- list()
+input$country <- "AT"
+# Possible expressions: EUR, PPS, NAC
+input$currency <- "EUR"
+
+# PC: Percentage, THS_PER: in thousands of persons
+input$value <- "PC"
+
+# Below 60% of median income
+input$threshold <- "LI_R_MD60"
+
+
+# Download the data from Eurostat
+arpr <- get_eurostat("ilc_li02")
+
+# Rewrite the date to year
+arpr$time <- year(arpr$time)
+
+# Country subset
+arpr <- arpr %>%
+  filter(geo == input$country,
+         unit == input$value,
+         indic_il == input$threshold,
+         age == "TOTAL",
+         sex == "T")
+
+p <- plot_ly(arpr, x = ~time, y = ~values, type = "scatter", mode = "lines") %>%
+  layout(title = "At risk of poverty rate over time", 
+         xaxis = list(title = "Year"),
+         yaxis = list(title = "At risk of poverty rate"))
+
+p
